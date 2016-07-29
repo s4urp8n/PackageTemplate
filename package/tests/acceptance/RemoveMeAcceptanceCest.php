eb0e208685a9516c836cbebda36ffa8145702a64
s4urp8n@yandex.ru <s4urp8n@yandex.ru>
2016-07-29 21:08:23 +0300

    totally refactoreed initial

--- package/tests/acceptance/RemoveMeAcceptanceCest.php
+++ package/tests/acceptance/RemoveMeAcceptanceCest.php
@@ -3,16 +3,18 @@
 class RemoveMeAcceptanceCest
 {
     
+    protected $host = 'http://127.0.0.1:4444/';
+    
     public function youCanRemoveThisTest1(AcceptanceTester $I)
     {
-        $I->amOnPage('http://127.0.0.1:4444/page1.php');
+        $I->amOnPage($this->host . 'page1.php');
         $I->seeResponseCodeIs(200);
         $I->see('Page1');
     }
     
     public function youCanRemoveThisTest2(AcceptanceTester $I)
     {
-        $I->amOnPage('http://127.0.0.1:4444/page2.php');
+        $I->amOnPage($this->host . 'page2.php');
         $I->seeResponseCodeIs(200);
         $I->see('Page2');
     }
