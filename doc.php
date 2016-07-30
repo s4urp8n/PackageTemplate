6be9ff77243f0c52b8f016a6f9936a6f6c1e6b78
s4urp8n@yandex.ru <s4urp8n@yandex.ru>
2016-07-30 15:24:58 +0300

    docs auto update

---
+++ doc.php
@@ -0,0 +1,44 @@
+<?php
+
+//Composer classes
+include 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+//Load configuration
+$config = include 'config.php';
+
+$commands = [
+    [
+        'description' => 'Package documenting using Apigen started...',
+    ],
+    [
+        'callback' => function () use ($config)
+        {
+            chdir(__DIR__);
+        },
+    ],
+    [
+        'callback' => function () use ($config)
+        {
+            $removes = [
+                'docs',
+            ];
+            
+            foreach ($removes as $remove)
+            {
+                PackageTemplate\removePath($remove);
+            }
+        },
+    ],
+    [
+        'command' => 'php apigen.phar generate --source package/src --destination docs/ --template-theme bootstrap',
+    ],
+    [
+        'callback' => function () use ($config)
+        {
+            passthru('git add docs');
+        },
+    ],
+];
+
+//Executing commands and show output
+PackageTemplate\executeCommands($commands);
