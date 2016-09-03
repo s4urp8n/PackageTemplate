a2c92937c32d4e4601ec58e285c4330c80c6df62
s4urp8n@yandex.ru <s4urp8n@yandex.ru>
2016-09-04 01:49:56 +0300

    changed to PHPUnit

---
+++ src/Functions.php
@@ -0,0 +1,5 @@
+<?php
+function removeMeFunction($arg1, $arg2)
+{
+    return 1;
+}
---
+++ src/ModifyMe.php
@@ -0,0 +1,16 @@
+<?php
+
+class ModifyMe
+{
+    
+    public static function method1($arg1 = 0, $arg2 = null)
+    {
+        return 1;
+    }
+    
+    public static function method2()
+    {
+        return 2;
+    }
+    
+}
