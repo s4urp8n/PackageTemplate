071aa68f1eccc69de28b96a519e4983a9c50a214
s4urp8n@yandex.ru <s4urp8n@yandex.ru>
2016-09-04 01:54:03 +0300

    changed to PHPUnit

---
+++ tests/ModifyMe2Test.php
@@ -0,0 +1,67 @@
+<?php
+
+class ModifyMe2Test extends PHPUnit\Framework\TestCase
+{
+    
+    public static function autoload()
+    {
+        $srcDirectory =
+            realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src') . DIRECTORY_SEPARATOR;
+        
+        //PSR4 autoloader
+        spl_autoload_register(
+            function ($className) use ($srcDirectory)
+            {
+                
+                $className = mb_eregi_replace('[\\\/]+', DIRECTORY_SEPARATOR, $className);
+                $className = mb_eregi_replace('^' . preg_quote(DIRECTORY_SEPARATOR) . '+', '', $className);
+                $className = mb_eregi_replace(preg_quote(DIRECTORY_SEPARATOR) . '+$', '', $className);
+                $className .= '.php';
+                
+                if (file_exists($srcDirectory . $className))
+                {
+                    include_once($srcDirectory . $className);
+                }
+                
+            }, false
+        );
+        
+        //Functions
+        $aliases = [
+            'func',
+            'functions',
+            'Functions',
+        ];
+        
+        $currentFile = '';
+        foreach ($aliases as $alias)
+        {
+            $currentFile = $srcDirectory . $alias . '.php';
+            if (file_exists($currentFile))
+            {
+                include_once($currentFile);
+            }
+        }
+    }
+    
+    public static function setUpBeforeClass()
+    {
+        static::autoload();
+    }
+    
+    public static function tearDownAfterClass()
+    {
+        
+    }
+    
+    public function testMethod1()
+    {
+        $this->assertSame(ModifyMe::method1(), 1);
+    }
+    
+    public function testMethod2()
+    {
+        $this->assertSame(ModifyMe::method2(), 2);
+    }
+    
+}
